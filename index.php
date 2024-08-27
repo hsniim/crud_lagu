1b8ef66c6ef50844b6532cc92bf477da4fe74671
Husni <husnovember21@gmail.com>
2024-08-27 23:58:39 +0700

    commit crud lagu

---
+++ index.php
@@ -0,0 +1,208 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$pass       = "";
+$db         = "lagu";
+
+$koneksi    = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //cek koneksi
+    die("Tidak bisa terkoneksi ke database");
+}
+$nil        = "";
+$nama_lagu       = "";
+$artis     = "";
+$album   = "";
+$sukses     = "";
+$error      = "";
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if($op == 'delete'){
+    $id         = $_GET['id'];
+    $sql1       = "delete from detail_lagu where id = '$id'";
+    $q1         = mysqli_query($koneksi,$sql1);
+    if($q1){
+        $sukses = "Berhasil hapus data";
+    }else{
+        $error  = "Gagal melakukan delete data";
+    }
+}
+if ($op == 'edit') {
+    $id         = $_GET['id'];
+    $sql1       = "select * from detail_lagu lagu id = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    $r1         = mysqli_fetch_array($q1);
+    $nil        = $r1['nim'];
+    $nama_lagu       = $r1['nama_lagu'];
+    $artis     = $r1['artis'];
+    $album   = $r1['album'];
+
+    if ($nil == '') {
+        $error = "Data tidak ditemukan";
+    }
+}
+if (isset($_POST['simpan'])) { //untuk create
+    $nil        = $_POST['nil'];
+    $nama_lagu       = $_POST['nama_lagu'];
+    $artis     = $_POST['artis'];
+    $album   = $_POST['album'];
+
+    if ($nil && $nama_lagu && $artis && $album) {
+        if ($op == 'edit') { //untuk update
+            $sql1       = "update detail_lagu set nil = '$nil',nama_lagu='$nama_lagu',artis = '$artis',album ='$album' where id = '$id'";
+            $q1         = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error  = "Data gagal diupdate";
+            }
+        } else { //untuk insert
+            $sql1   = "insert into mahasiswa(nil,nama_lagu,artis,album) values ('$nil','$nama_lagu','$artis','$album')";
+            $q1     = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses     = "Berhasil memasukkan data baru";
+            } else {
+                $error      = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Silakan masukkan semua data";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        .mx-auto {
+            width: 800px
+        }
+
+        .card {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="mx-auto">
+        <!-- untuk memasukkan data -->
+        <div class="card">
+            <div class="card-header">
+                Create / Edit Data
+            </div>
+            <div class="card-body">
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php");//5 : detik
+                }
+                ?>
+                <?php
+                if ($sukses) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $sukses ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php");
+                }
+                ?>
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="nim" class="col-sm-2 col-form-label">NIL</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nil" name="nil" value="<?php echo $nil ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="nama_lagu" class="col-sm-2 col-form-label">Nama Lagu</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nama_lagu" name="nama_lagu" value="<?php echo $nama_lagu ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="artis" class="col-sm-2 col-form-label">Artis</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="artis" name="artis" value="<?php echo $artis ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="album" class="col-sm-2 col-form-label">Album</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="album" name="album" value="<?php echo $album ?>">
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <!-- untuk mengeluarkan data -->
+        <div class="card">
+            <div class="card-header text-white bg-secondary">
+                Data Mahasiswa
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NIL</th>
+                            <th scope="col">Nama Lagu</th>
+                            <th scope="col">Artis</th>
+                            <th scope="col">Album</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql2   = "select * from detail_lagu order by id desc";
+                        $q2     = mysqli_query($koneksi, $sql2);
+                        $urut   = 1;
+                        while ($r2 = mysqli_fetch_array($q2)) {
+                            $id         = $r2['id'];
+                            $nil        = $r2['nil'];
+                            $nama_lagu       = $r2['nama_lagu'];
+                            $artis     = $r2['artis'];
+                            $album   = $r2['album'];
+
+                        ?>
+                            <tr>
+                                <th scope="row"><?php echo $urut++ ?></th>
+                                <td scope="row"><?php echo $nim ?></td>
+                                <td scope="row"><?php echo $nama ?></td>
+                                <td scope="row"><?php echo $alamat ?></td>
+                                <td scope="row"><?php echo $fakultas ?></td>
+                                <td scope="row">
+                                    <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-warning">Edit</button></a>
+                                    <a href="index.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau hapus?')"><button type="button" class="btn btn-danger">Hapus</button></a>            
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
